fd735e9f06dcce34160a6c5eac869a5c5666f2fa
primecodez <narendrakumarsingh2859@gmail.com>
2026-01-06 17:42:47 +0000

    added

--- ps/passgen.py
+++ ps/passgen.py
@@ -2,10 +2,10 @@ import random
 import string
 
 def password_generator(length=10):
-    """creation a functu=ion that will generate password randomly using random library of python"""
+    """Create a function that generates a random password using Python's random library."""
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-    print(password_generator(8))
-    
+
+if __name__ == "__main__":    print(password_generator(8))
